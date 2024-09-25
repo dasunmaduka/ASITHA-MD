a34a25218ef184081570d60ad859a9807d25708d
dasunmaduka <125837525+dasunmaduka@users.noreply.github.com>
2024-09-26 04:56:28 +0530

    Update config.js
    Hvbm

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "5i9AhT5I#DwXEt6KNVQJRubMrO9icQTP-APq72hpusCl1Rjvr970",
+MONGODB: process.env.MONGODB || "5i9AhT5I#DwXEt6KNVQJRubMrO9icQTP-APq72hpusCl1Rjvr970",//enter mongo db url
 };
